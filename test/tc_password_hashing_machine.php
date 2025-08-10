843ff93400a5050db785b9d5c3491aa9849403cd
Jaromir Tomek <jaromir.tomek@gmail.com>
2025-08-10 22:37:47 +0200

    Test fixed (?)

--- test/tc_password_hashing_machine.php
+++ test/tc_password_hashing_machine.php
@@ -91,7 +91,7 @@ class TcPasswordHashingMachine extends TcBase {
 		$phm = new Yarri\PasswordHashingMachine();
 		$phm->addAlgorithm(
 			function($password){ return password_hash($password,PASSWORD_BCRYPT); },
-			function($password){ return !password_needs_rehash($password,PASSWORD_BCRYPT); },
+			function($password){ return !password_needs_rehash($password,PASSWORD_BCRYPT,["cost" => 10]); },
 			function($password,$hash){ return password_verify($password,$hash); }
 		);
 
